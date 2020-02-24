4a8cb91448fb9475bc61f88764e0c2a2746be297
luaifei <lu.aifei@thoughtworks.com>
2020-02-24 09:42:35 +0800

    1. 统一代码风格，tab转换为4个空格

--- src/main/java/cc/xpbootcamp/warmup/cashier/GoodsItem.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/GoodsItem.java
@@ -8,17 +8,17 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Getter
 public class GoodsItem {
-	private static final String FORMATTER = "%s, %.2f x %s, %.2f\n";
+    private static final String FORMATTER = "%s, %.2f x %s, %.2f\n";
 
-	private String description;
-	private BigDecimal price;
-	private int quantity;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
 
     BigDecimal calculateSubTotalExcludeTax() {
         return price.multiply(BigDecimal.valueOf(quantity));
     }
 
     String formatGoodsItem() {
-		return String.format(FORMATTER, description, price, quantity, calculateSubTotalExcludeTax());
-	}
+        return String.format(FORMATTER, description, price, quantity, calculateSubTotalExcludeTax());
+    }
 }
